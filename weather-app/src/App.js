d274c24573475d7439ca14c91a8bfcc32266f491
Lisa Deshishku <l.deshishku@se22.qmul.ac.uk>
2024-03-13 16:51:36 +0000

    css changes

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,12 +1,15 @@
 // App.js
 import React from 'react';
 import WeatherApp from './Components/WeatherApp';
+import MarineWeather from './Components/marineWeather';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <WeatherApp/>
+        <MarineWeather/>
       </header>
     </div>
   );
