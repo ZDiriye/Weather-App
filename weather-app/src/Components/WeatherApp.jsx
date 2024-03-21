059d230e2df70f45642b05ddd90176b5309a87f3
Zakariye Diriye <zakariyediriye@zakariyes-air.wkstn.qmul.ac.uk>
2024-03-21 14:51:44 +0000

    wind-map-integration

--- weather-app/src/Components/WeatherApp.jsx
+++ weather-app/src/Components/WeatherApp.jsx
@@ -8,21 +8,26 @@ import location from "./Assets/location.png";
 import waves from "./Assets/Waves.png";
 import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 
+
 const WeatherApp = () => {
+  // State variables
   const [weatherData, setWeatherData] = useState(null);
   const [hourlyForecast, setHourlyForecast] = useState([]);
-  const [city, setCity] = useState('Cape Town'); // Initialize city as a state variable
+  const [city, setCity] = useState('Cape Town'); // Initialise city as a state variable
   const [searchInput, setSearchInput] = useState(''); // State for handling search input
   const apiKey = '2cc31d01c769fda3dbadec4c1b5f8185';
   const [CSSclass, setCSSclass] = useState('weather-container-day');
 
-
+  
   useEffect(() => {
+    // Fetch weather data and forecast
     const fetchData = async () => {
       try {
+        // Fetch current weather data
         const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
         setWeatherData(weatherResponse.data);
 
+        // Fetch hourly forecast data
         const forecastResponse = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`);
         const hourlyData = forecastResponse.data.list.slice(0, 5).map(hour => ({
           time: hour.dt_txt.substr(11, 5),
@@ -35,11 +40,14 @@ const WeatherApp = () => {
       }
     };
 
+
     let currentDate = new Date();
     let currentHour = currentDate.getHours();
 
+
     console.log(currentHour);
 
+    // Determine CSS class based on time of day
     if(currentHour >= 7 && currentHour <= 19){
       setCSSclass('weather-container-day');
     }
@@ -47,19 +55,21 @@ const WeatherApp = () => {
       setCSSclass('weather-container-night');
     }
 
+
     fetchData();
   }, [apiKey, city]); // Depend on city to refetch data when it changes
 
+  // Handle search input change
   const handleSearch = (e) => {
     e.preventDefault(); // Prevent the default form submit action
     setCity(searchInput); // Update the city state with the user's input
   };
 
+  // Map options for Google Maps
   const mapOptions = { gestureHandling: 'greedy', disableDefaultUI: true };
-  
 
+  // Render weather app UI
   return (
-
       <div className={CSSclass}>
         <div className='searchBar'>
         <form onSubmit={handleSearch}>
@@ -112,18 +122,24 @@ const WeatherApp = () => {
                 </div>
               </APIProvider>
             </div>
+
+
           </>
         )}
         <div className="navigation-bar">
-          <Link to="/wind" state={weatherData ? weatherData.name:"Cape Town"}><img src={wind} alt="Wind" className="nav-icon" /></Link>
+            <Link to="/wind" state={[weatherData ? weatherData.name:"Cape Town", weatherData ? weatherData.coord.lat:-33.92584, weatherData ? weatherData.coord.lon:18.42322]}><img src={wind} alt="Wind" className="nav-icon" /></Link>
 
-          <Link to="/"><img src={location} alt="Locations" className="nav-icon" /></Link>
 
-          <Link to="/waves" state={weatherData ? weatherData.name:"Cape Town"}><img src={waves} alt="Waves" className="nav-icon" /></Link>
+            <Link to="/"><img src={location} alt="Locations" className="nav-icon" /></Link>
 
-        </div>
+
+            <Link to="/waves" state={weatherData ? weatherData.name:"Cape Town"}><img src={waves} alt="Waves" className="nav-icon" /></Link>
+
+
+          </div>
       </div>
   );
-};
+  };
 
-export default WeatherApp;
+
+  export default WeatherApp;
